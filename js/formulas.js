cb2a2548f801454de6fa3d976913baf33731bc39
koggenfuss <koggenfuss@gmail.comllsgit config --global user.email koggenfuss@gmail.com>
2021-01-01 09:29:41 -0500

    final commit

--- js/formulas.js
+++ js/formulas.js
@@ -1,80 +1,94 @@
 // Basic math formulaas
 function addition(num1, num2){
-  return -1;
+  let add = num1 + num2;
+  return add;
 }
 
 function subtraction(num1, num2){
-  return -1;
+  let sub = num1 - num2;
+  return sub;
 }
 
 function multiplication(num1, num2){
-  return -1;
+  let mult = num1 * num2;
+  return mult;
 }
 
 function division(num1, num2){
-  return -1;
+  let div = num1  / num2;
+  return div;
 }
 
 // Area formulaas
 function areaSquare(side){
-  return -1;
+  let aS = Math.pow(side, 2);
+  return aS;
 }
 
 function areaRectangle(length, width){
-  return -1;
+  let aR = length * width;
+  return aR;
 }
 
 function areaParallelogram(base, height){
-  return -1;
+  let aP = base * height;
+  return aP;
 }
 
 function areaTriangle(base, height){
-  return -1;
+  let aT =  (base * height) / 2;
+  return aT;
 }
 
 function Circle(radius){
-  return -1;
-}
-
-function Sphere(radius){
-  return -1;
+  let circum = Math.pow((3.14 * radius), 2);
+  return circum;
 }
 
 // Surface Area formulas
 function surfaceAreaCube(side){
-  return -1;
+  let surfAC = Math.pow((6 * side), 2)
+  return surfAC;
 }
 
 function surfaceAreaCylinder(radius, height){
-  return -1;
+  let surfAreaCyl = Math.pow((3.14 * radius * height), 2);
+  return surfAreaCyl;
 }
 
 // Perimeter formulas
 function perimeterSquare(side){
-  return -1;
+  let perSquare = Math.pow(side, 4);
+  return perSquare;
 }
 
 function perimeterRectangle(length, height){
-  return -1;
+  let perRect = Math.pow((length * height), 2)
+  return perRect;
 }
 
 function perimeterTriangle(side1, side2, side3){
-  return -1;
+  let perTri = (side1 + side2 + side3);
+  return perTri;
 }
 
 function perimeterCircle(diameter){
-  return -1;
+  let perCir = 3.14 * diameter;
+  return perCir;
 }
 
 // Volume formulas
 function volumeCube(side){
-  return -1;
+  let volCube = Math.pow(side, 3);
+  return volCube;
 }
 
 function volumeRectangular(length, width, height){
-  return -1;
+  let volRect = length * width * height;
+  return volRect;
 }
 
 function volumeCylinder(radius, height){
-  return -1;
+  let volCyl = Math.pow((3.14 * radius), 2);
+  return volCyl;
 }
